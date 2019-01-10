d8b71392f4b7e2b180d903814d3518e20a675e5f
Yobany <yobanydesign@gmail.com>
2019-01-10 09:31:45 -0500

    Added get and post methods for api/friends.

---
+++ FriendFinder/app/routing/apiRoutes.js
@@ -0,0 +1,20 @@
+// Pull in required dependencies
+var path = require('path');
+
+// Import the list of friend entries
+var friends = require('../data/friends.js');
+
+// Export API routes
+module.exports = function(app) {
+	// console.log('___ENTER apiRoutes.js___');
+
+	// Total list of friend entries
+	app.get('/api/friends', function(req, res) {
+		res.json(friends);
+    });
+    
+    // Add new friend entry
+	app.post('/api/friends', function(req, res) {
+		// Capture the user input object
+		var userInput = req.body;
+		// console.log('userInput = ' + JSON.stringify(userInput));
